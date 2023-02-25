0451e6e53afdfd22d8bb9884fc68e1580674bc41
zamzoki <zanoschi.david@gmail.com>
2023-02-25 21:41:24 +0200

    implement script

---
+++ append.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while getopts 't:f:h' OPTION; do
+  case "$OPTION" in
+    t)
+      TEXT="$OPTARG"
+      ;;
+    f)
+      FILE_PATH="$OPTARG"
+      ;;
+    h)
+      echo "-t      text to append"
+      echo "-f      file path"
+      exit 0
+      ;;
+    *)
+      echo "Invalid flag provided." >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo $TEXT >> $FILE_PATH
